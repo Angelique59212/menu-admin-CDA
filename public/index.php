d87f15780575e7c5e9af742eb49f340ec42d0d7c
angel <dehainaut.angelique@orange.fr>
2022-11-02 11:53:57 +0100

    menu

---
+++ public/index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Menu administrateur</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="container">
+        <div id="left">
+            <div class="title">
+                <i class="fa-solid fa-spinner"></i>
+                <h1 id="title">Basik</h1>
+            </div>
+            <div class="title">
+                <h2 id="main">Main</h2>
+                <i class="fa-solid fa-house"></i> <span>Dashboard</span>
+            </div>
+            <div class="title">
+                <h2>Applications</h2>
+                <div>
+                    <i class="fa-regular fa-calendar"></i> <a href="/" class="appli">Calendar</a>
+                </div>
+                <div>
+                    <i class="fa-regular fa-user"></i> <p class="appli">Users</p>
+                </div>
+                <div>
+                    <i class="fa-regular fa-message"></i> <a href="/" class="appli">Message</a>
+                </div>
+                <div>
+                    <i class="fa-regular fa-envelope"></i> <p class="appli">E-mail</p>
+                </div>
+            </div>
+            <div class="title">
+                <h2>UI elements</h2>
+                <div>
+                    <div>
+                        <i class="fa-solid fa-cubes"></i> <a href="https://www.google.fr" class="appli">Components</a>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-cube"></i> <p class="appli">Plugins</p>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-circle-check"></i> <p class="appli">Form</p>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-list"></i> <p class="appli">Tables</p>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-clock"></i> <p class="appli">Charts</p>
+                    </div>
+                </div>
+            </div>
+            <div class="title">
+                <h2>Extras</h2>
+                <div>
+                    <i class="fa-regular fa-file"></i> <a href="/" class="appli">Pages</a>
+                </div>
+                <div>
+                    <i class="fa-regular fa-user"></i> <a href="/" class="appli">Auth</a>
+                </div>
+            </div>
+        </div>
+        <div id="right"></div>
+    </div>
+
+    <script src="https://kit.fontawesome.com/9fe3f13f0a.js" crossorigin="anonymous"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
